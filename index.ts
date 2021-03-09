1aae07b293de7e87981203a87724ee7cfc2aecb2
Kitiyaparnnn <parn.takhum@gmail.com>
2021-03-09 14:06:42 +0700

    register finish

--- index.ts
+++ index.ts
@@ -1,81 +1,111 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import { body, query, validationResult } from 'express-validator'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { body, query, validationResult } from "express-validator";
+import fs from "fs";
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 3000
-const SECRET = "SIMPLE_SECRET"
+const PORT = process.env.PORT || 3000;
+const SECRET = "SIMPLE_SECRET";
 
 interface JWTPayload {
   username: string;
   password: string;
 }
 
-app.post('/login',
-  (req, res) => {
-
-    const { username, password } = req.body
-    // Use username and password to create token.
-
-    return res.status(200).json({
-      message: 'Login succesfully',
-    })
-  })
-
-app.post('/register',
-  (req, res) => {
-
-    const { username, password, firstname, lastname, balance } = req.body
-  })
-
-app.get('/balance',
-  (req, res) => {
-    const token = req.query.token as string
-    try {
-      const { username } = jwt.verify(token, SECRET) as JWTPayload
-  
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  // Use username and password to create token.
+  const file = JSON.parse(
+    fs.readFileSync("./user.json", { encoding: "utf-8" })
+  );
+  const { users } = file;
+  const complete = users.find(
+    (user: { Username: string; password: string }) => {
+      user.Username === username && bcrypt.compareSync(password, user.password);
     }
-    catch (e) {
-      //response in case of invalid token
-    }
-  })
+  );
+  if (complete) {
+    const token = jwt.sign(
+      {
+        username: complete.username,
+      },
+      SECRET
+    );
+    res.status(200).json({
+      massage: "Login succesfully",
+      token,
+    });
+  } else {
+    res.status(400).json({
+      massage: "Invalid username or password",
+    });
+  }
+  res.end();
+});
 
-app.post('/deposit',
-  body('amount').isInt({ min: 1 }),
-  (req, res) => {
+app.post("/register", (req, res) => {
+  const { username, password, firstname, lastname, balance } = req.body;
+  const file = JSON.parse(
+    fs.readFileSync("./user.json", { encoding: "utf-8" })
+  );
+  const { users } = file;
+  const existUser = users.find(
+    (user: { username: string }) => user.username === username
+  );
+  if (existUser) {
+    res.status(400).json({ massage: "Username is already in used" });
+  } else {
+    const encryptPassword = bcrypt.hashSync(password, 10);
+    const newUser = {
+      username,
+      password: encryptPassword,
+      firstname,
+      lastname,
+      balance,
+    };
+    users.push(newUser);
+    fs.writeFileSync("./user.json", JSON.stringify(file));
+    res.status(200).json({ massage: "Register successfully" });
+  }
+});
 
-    //Is amount <= 0 ?
-    if (!validationResult(req).isEmpty())
-      return res.status(400).json({ message: "Invalid data" })
-  })
+app.get("/balance", (req, res) => {
+  const token = req.query.token as string;
+  try {
+    const { username } = jwt.verify(token, SECRET) as JWTPayload;
+  } catch (e) {
+    //response in case of invalid token
+  }
+});
 
-app.post('/withdraw',
-  (req, res) => {
-  })
+app.post("/deposit", body("amount").isInt({ min: 1 }), (req, res) => {
+  //Is amount <= 0 ?
+  if (!validationResult(req).isEmpty())
+    return res.status(400).json({ message: "Invalid data" });
+});
 
-app.delete('/reset', (req, res) => {
+app.post("/withdraw", (req, res) => {});
 
+app.delete("/reset", (req, res) => {
   //code your database reset here
-  
+
   return res.status(200).json({
-    message: 'Reset database successfully'
-  })
-})
+    message: "Reset database successfully",
+  });
+});
 
-app.get('/me', (req, res) => {
-  
-})
+app.get("/me", (req, res) => {});
 
-app.get('/demo', (req, res) => {
+app.get("/demo", (req, res) => {
   return res.status(200).json({
-    message: 'This message is returned from demo route.'
-  })
-})
+    message: "This message is returned from demo route.",
+  });
+});
 
-app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
